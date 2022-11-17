817931db67c5a30f354adbbfa4316eaf9c71ed46
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-11-17 17:43:42 +0100

    initialize project -gpt-3

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      f8: '62px',
+      f7: '34px',
+      f6: '24px',
+      f5: '20px',
+      f4: '18px',
+      f3: "16px",
+      f2: "14px",
+      f1: "12px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
